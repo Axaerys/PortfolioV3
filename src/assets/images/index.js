2e92881711d1aef9bf7b502a9d668f02b1cc56cc
Axaerys <seyed6635@gmail.com>
2025-03-09 15:03:40 +0400

    added fixsavvy

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,10 +2,12 @@ import myPic from "./myPic.jpg";
 import AquaSerene from "./AquaSerene.png";
 import IceBerg from "./IceBerg.png";
 import Qike from "./Qike.png";
+import FixSavvy from "./FixSavvy.png";
 
 export {
     myPic,
     AquaSerene,
     IceBerg,
     Qike,
+    FixSavvy,
 }
